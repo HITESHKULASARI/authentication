813d3d63a685f2ee5f0fc6704a77293b62da9ff0
HITESH KULASARI <hiteshkulasari@gmail.com>
2023-08-10 17:41:21 +0530

    setting up the router

---
+++ routers/index.js
@@ -0,0 +1,10 @@
+//importing express
+const express = require('express');
+
+//using router function
+const router =  express.Router();
+
+console.log("router is working");
+
+
+module.exports = router;
